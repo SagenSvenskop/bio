0ada76dff0c41e5187ac2607ce9f32e56e27bead
= <linne.louise@gmail.com>
2024-08-26 23:09:49 +0200

    Fix booking button logic

--- javascript/displayEvents.js
+++ javascript/displayEvents.js
@@ -5,7 +5,7 @@ let eventsContainer = document.getElementById('events');
 function addEventLinks(data, container) {
     for (let i = 0; i < data.length; i++) {
         let element = document.createElement("div");
-        let isBookable = !data[i]["book"]
+        let isBookable = !!data[i]["book"];
         element.innerHTML = isBookable 
           ? `<p><a href="${data[i]["url"]}">${data[i]["place"]}</a> ${data[i]["time"]} <a class="button" href="${data[i]["book"]}">Boka</a> </p>`
           : `<p><a href="${data[i]["url"]}">${data[i]["place"]}</a> ${data[i]["time"]} </p>`;
