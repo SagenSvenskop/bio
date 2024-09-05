bbabda71cfa41b356fccf0e740632ebd6159094c
Louise Linné <linne.louise@gmail.com>
2024-09-05 13:28:11 +0200

    Detail summary format for movies with trailers and desc

--- javascript/displayEvents.js
+++ javascript/displayEvents.js
@@ -3,20 +3,27 @@ let previousContainer = document.getElementById('previous-screenings');
 let eventsContainer = document.getElementById('events');
 
 function addEventLinks(data, container) {
-    for (let i = 0; i < data.length; i++) {
-        let element = document.createElement("div");
-        let isBookable = !!data[i]["book"];
-        element.innerHTML = isBookable 
-          ? `<p><a href="${data[i]["url"]}">${data[i]["place"]}</a> ${data[i]["time"]} <a class="button" href="${data[i]["book"]}">Boka</a> </p>`
-          : `<p><a href="${data[i]["url"]}">${data[i]["place"]}</a> ${data[i]["time"]} </p>`;
-        container.appendChild(element);
-    };
+  for (let i = 0; i < data.length; i++) {
+    const info = data[i];
+    const element = document.createElement('div');
+    const bookHTML = !!info.book
+      ? `<div><a class="button" href="${info.book}">Boka</a></div`
+      : '';
+    const summaryHTML = `<span class=summary-title>${info.title || ''}</span> ${
+      info.time || ''
+    }`;
+    const detailsHTML = `<p>${
+      info.description || ''
+    }</p><div>${bookHTML}</div><div class=trailer>${info.trailer || ''}</div>`;
+    element.innerHTML = `<article><details><summary>${summaryHTML}</summary>${detailsHTML}</details><article>`;
+    container.appendChild(element);
+  }
 }
 
-let res = fetch("./data/events.json")
-    .then((response) => response.json())
-    .then((json) => {
-        screeningsContainer && addEventLinks(json.screenings, screeningsContainer);
-        previousContainer && addEventLinks(json.previous, previousContainer);
-        eventsContainer && addEventLinks(json.events, eventsContainer);
-    });
+let res = fetch('./data/events.json')
+  .then((response) => response.json())
+  .then((json) => {
+    screeningsContainer && addEventLinks(json.screenings, screeningsContainer);
+    previousContainer && addEventLinks(json.previous, previousContainer);
+    eventsContainer && addEventLinks(json.events, eventsContainer);
+  });
